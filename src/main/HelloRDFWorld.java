f4bbc05ab16d7445293b8caa521bacf5714d2681
u0115286 <ronald.teo@thomsonreuters.com>
2015-03-09 15:43:11 -0500

    print statements

--- src/main/HelloRDFWorld.java
+++ src/main/HelloRDFWorld.java
@@ -2,7 +2,11 @@ package main;
 
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
 import com.hp.hpl.jena.vocabulary.VCARD;
 
 
@@ -11,19 +15,43 @@ public class HelloRDFWorld {
 	public static void main(String[] args) {
 		
 		final String personURI = "http://somewhere/RonaldTeo";
-		final String fullName = "Ronald Teo";
 		final String givenName = "Ronald";
 		final String familyName = "Teo";
+		final String fullName = givenName + " " + familyName;
 
 		Model model = ModelFactory.createDefaultModel();
 		
 		Resource ronaldTeo = model.createResource(personURI)
 									.addProperty(VCARD.FN, fullName)
 									.addProperty(VCARD.N, model.createResource()
-											.addProperty(VCARD.Given, givenName)
-											.addProperty(VCARD.Family, familyName));
+									.addProperty(VCARD.Given, givenName)
+									.addProperty(VCARD.Family, familyName));
 		
-		model.write(System.out, "JSON-LD");
+
+		StmtIterator iter = model.listStatements();
+		
+		while(iter.hasNext()) {
+			Statement stmt = iter.nextStatement();
+			
+			Resource subject = stmt.getSubject();
+			Property predicate = stmt.getPredicate();
+			RDFNode object = stmt.getObject();
+			
+			System.out.print(subject.toString());
+			System.out.print(" " + predicate.toString() + " ");
+			
+		    if (object instanceof Resource) {
+		        System.out.print(object.toString());
+		     } else {
+		         // object is a literal
+		         System.out.print(" \"" + object.toString() + "\"");
+		     }
+		    
+		    System.out.println(" .");
+		}
+		
 	}
+	
+	
 
 }
